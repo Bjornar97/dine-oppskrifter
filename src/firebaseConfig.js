db4aba70718780615ab847b3666b850269bea4e4
Bjørnar Hetlesæther <hetlest@hotmail.com>
2019-08-27 12:45:28 +0200

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export var firebaseConfig = {
-  apiKey: "AIzaSyDFbYncXDkXyeINBgiO0IuE4aiVEWRiCv4",
-  authDomain: "dine-oppskrifter.firebaseapp.com",
-  databaseURL: "https://dine-oppskrifter.firebaseio.com",
-  projectId: "dine-oppskrifter",
-  storageBucket: "dine-oppskrifter.appspot.com",
-  messagingSenderId: "844888763481",
-  appId: "1:844888763481:web:33759056eff108a0"
-};
